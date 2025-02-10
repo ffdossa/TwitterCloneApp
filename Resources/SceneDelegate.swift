376992b51388bb447a0ccf70b74bca8ecb9221e8
ffdossa <127089701+ffdossa@users.noreply.github.com>
2025-02-10 12:12:49 +0200

    rename

--- Resources/SceneDelegate.swift
+++ Resources/SceneDelegate.swift
@@ -14,11 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = MainTabViewController()
         window?.makeKeyAndVisible()
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
